281ad117403c2668d61dbb2360c5dc5122d29273
nvtamnt99 <nvtamnt99@gmail.com>
2022-12-20 07:42:18 +0700

    feature class

---
+++ app/controllers/smart_classes_controller.rb
@@ -0,0 +1,56 @@
+class SmartClassesController < ApplicationController
+  before_action :set_smart_class, only: [:edit, :update, :show]
+  def index
+    @smart_classes = SmartClass.all
+  end
+
+  def new
+    @smart_class = SmartClass.new
+  end
+
+  def create
+    @smart_class = SmartClass.new(smart_class_params)
+
+    if @smart_class.save
+      redirect_to @smart_class, notice: 'Create class successfull'
+    else
+      flash[:alert] = 'Errors'
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+ # PATCH/PUT /leads/1
+ def update
+    if @smart_class.update(smart_class_params)
+      redirect_to @smart_class, notice: 'Update class successfull'
+    else
+      flash[:alert] = 'Errors'
+      render :edit
+    end
+  end
+
+  # DELETE /leads/1
+  def destroy
+    @smart_class.destroy
+    redirect_to smart_classes_path, notice: 'Destroy class successfull'
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_smart_class
+    @smart_class = SmartClass.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def smart_class_params
+    parameters_list = [
+      :class_code,
+      :name
+    ]
+
+    params.require(:smart_class).permit(parameters_list)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'dashboard#index'
-  resources :smart_class
+  resources :smart_classes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
