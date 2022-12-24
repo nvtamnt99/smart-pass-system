1d45a84c35da4d1fae2f93c6e90b72cac3b35776
nvtamnt99 <nvtamnt99@gmail.com>
2022-12-25 00:02:51 +0700

    Time record featur

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_20_085011) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_24_153841) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_20_085011) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["smart_class_id"], name: "index_students_on_smart_class_id"
+  end
+
+  create_table "time_records", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.bigint "student_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["student_id"], name: "index_time_records_on_student_id"
   end
 
   create_table "users", force: :cascade do |t|
