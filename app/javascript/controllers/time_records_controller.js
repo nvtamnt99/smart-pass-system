88e76d101c78c86e257c1878625ada3c3463c584
nvtamnt99 <nvtamnt99@gmail.com>
2022-12-25 15:17:04 +0700

    Timer

---
+++ app/javascript/controllers/time_records_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ ]
+
+  connect() {
+    $('.timers').each((index, e) => {
+      const studentId = $(e).data('student-id')
+      const startTime = $(e).data('start-time')
+      if (!!startTime) {
+        let sec = startTime;
+        function pad ( val ) { return val > 9 ? val : "0" + val; }
+        setInterval( function(){
+            $(`#second-${studentId}`).html(pad(++sec%60))
+            $(`#minutes-${studentId}`).html(pad(parseInt(sec/60,10)))
+        }, 1000)
+      }
+    })
+  }
+}
